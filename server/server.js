455c573cbde910117ecc4e50905767600926bc84
unknown <engr.ab20@gmail.com>
2019-08-24 14:57:48 +0500

    3rd commit

--- server/server.js
+++ server/server.js
@@ -5,11 +5,12 @@ const publicPath = path.join(__dirname,'../public');
 console.log(publicPath);
 
 const app = express();
+const port = process.env.PORT ||3000;
 
 //configure express static middleware to server a directory for views
 app.use(express.static(publicPath));
 
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log('server started at port 3000');
-})
+});
