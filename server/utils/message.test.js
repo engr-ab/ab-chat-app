e82e92c0693480d9e2b6ba1817e2d227083ada81
unknown <engr.ab20@gmail.com>
2019-09-06 16:14:56 +0500

    geolocation support test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,16 +1,24 @@
 var expect = require('expect');
-var {generateMessage} = require('./messages');
+var {generateMessage, generateLocationMessage} = require('./messages');
 
-describe('generateMessage',()=>{
+describe('generateMessage:',()=>{
     var from='admin';
     var text= 'test text';
 
     it('should generate correct message object',()=>{
         var message = generateMessage(from,text);
         expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, text});   
+    });
+});
 
-        expect(message).toInclude({from, text});
-        
-        
-    })
+describe('generateLocationMessage:', ()=>{
+    var from = 'ab'; var lat = 15; var lan = 16; 
+    var url = `https://www.google.com/maps?q=${lat},${lan}`;
+    var message = generateLocationMessage(from, lat, lan);
+
+    it('Should generate correct location object',()=>{
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, url});
+    });
 });
